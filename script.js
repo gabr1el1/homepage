325d7bb749f3af886d28491e62883ed044eefc43
gabr1el1 <gabrielhgxd@gmail.com>
2024-01-11 10:40:20 -0600

    cards

---
+++ script.js
@@ -0,0 +1,58 @@
+let carousels = document.querySelectorAll(".carousel");
+class Carousel {
+  constructor(carousel) {
+    this.carousel = carousel;
+    this.images = carousel.querySelectorAll("img");
+    this.lengthCarousel = this.images.length;
+
+    this.indexActive = 0;
+
+    this.carousel.addEventListener("click", this.enableCarousel.bind(this));
+
+    this.left = this.carousel.querySelector(".left");
+    this.left.addEventListener("click", this.previous.bind(this));
+
+    this.right = this.carousel.querySelector(".right");
+    this.right.addEventListener("click", this.next.bind(this));
+
+    this.closeBtn = this.carousel.querySelector(".close");
+    this.closeBtn.addEventListener("click", this.close.bind(this));
+  }
+  close(event) {
+    event.stopPropagation();
+    this.carousel.classList.remove("active");
+    if (this.indexActive !== 0) {
+      this.images[this.indexActive].classList.remove("active");
+      this.images[0].className = "active";
+    }
+  }
+  enableCarousel(event) {
+    if (!this.carousel.classList.contains("active")) {
+      this.carousel.classList.add("active");
+    }
+  }
+
+  previous() {
+    this.images[this.indexActive].classList.remove("active");
+    if (this.indexActive > 0) {
+      this.indexActive -= 1;
+    } else {
+      this.indexActive = this.lengthCarousel - 1;
+    }
+
+    this.images[this.indexActive].classList.add("active");
+  }
+  next() {
+    this.images[this.indexActive].classList.remove("active");
+    if (this.indexActive < this.lengthCarousel - 1) {
+      this.indexActive += 1;
+    } else {
+      this.indexActive = 0;
+    }
+
+    this.images[this.indexActive].classList.add("active");
+  }
+}
+carousels.forEach(function (carousel) {
+  new Carousel(carousel);
+});
